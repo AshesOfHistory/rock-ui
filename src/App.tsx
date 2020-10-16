57637a568bc865fe752a3bf022e9869ff8c69e1b
wangpeng_f <wangpeng_f@aspirecn.com>
2020-10-16 13:09:10 +0800

    按钮的样式添加

--- src/App.tsx
+++ src/App.tsx
@@ -9,12 +9,15 @@ function App() {
       <h1>hello worle</h1>
       <h2>hello worle</h2>
       <h3>hello worle</h3>
-
-      <Button disabled>default</Button>
-      <Button btnType={ButtonType.Primary} size={ButtonSize.Large}>primary</Button>
+      <Button>default</Button>
+      <Button disabled>default disabled</Button>
+      <Button btnType={ButtonType.Primary} size={ButtonSize.Large}>primary large</Button>
+      <Button btnType={ButtonType.Danger
+      } size={ButtonSize.Small}>danger small</Button>
+      <Button btnType={ButtonType.Link} href="http://www.baidu.com">link default</Button>
       <Button disabled btnType={ButtonType.Link}
         href="http://www.baidu.com"
-      >link</Button>
+      >link disabled</Button>
     </div>
   );
 }
