316f4f6a3c43c5d267f1e8141012594319bd0229
王鹏 <wangpeng_f@aspirecn.com>
2021-02-20 14:37:40 +0800

    利用plop创建组件

--- src/components/Button/button.test.tsx
+++ src/components/Button/button.test.tsx
@@ -1,9 +1,26 @@
-import React from  'react'
-import {render} from '@testing-library/react'
+import React from 'react'
+import { render } from '@testing-library/react'
 import Button from './button'
 
 test('out first react test case', () => {
   const wrapper = render(<Button>Nice</Button>)
   const element = wrapper.queryByText('Nice')
   expect(element).toBeTruthy()
+  expect(element).toBeInTheDocument()
+})
+
+describe('test Button component', () => {
+  it('should render the correct default button', () => {
+    const wrapper = render(<Button>Nice</Button>)
+    const element = wrapper.getByText('Nice')
+    expect(element).toBeInTheDocument()
+    expect(element.tagName).toEqual('BUTTON')
+    expect(element).toHaveClass('btn btn-default')
+  })
+  it('should render the correct component base on different props', () => {
+
+  })
+  it('should render a link when btntype equals link and link is provided', () => {
+
+  })
 })
